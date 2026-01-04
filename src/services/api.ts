604a441777bed21d559c947df3eca528b6764397
IreshChathuranga <ireshchathuranga63309.com>
2026-01-04 10:28:49 +0530

    add new baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { refreshTokens } from "./auth";
 
 const api = axios.create({
-  baseURL: "https://salon-boking-system-be.vercel.app/api/v1",
+  baseURL: "https://salon-boking-system-be.vercel.app/",
 });
 
 const PUBLIC_ENDPOINTS = ["/user/login", "/user/register", "/user/refreshtoken"];
